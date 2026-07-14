94eae26b3d2e4cbf35b375ec32b01dddbb6b9b6b
kaviyabanuka28-source <kaviyabanuka28@gmail.com>
2026-07-14 14:54:18 +0530

    Update node.js

--- node.js
+++ node.js
@@ -51,6 +51,7 @@ const client = new Client({
     }),
     puppeteer: {
         headless: true,
+        executablePath: '/usr/bin/chromium', 
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
