2796b74387390beb829f20c6ff62842a627be464
kaviyabanuka28-source <kaviyabanuka28@gmail.com>
2026-07-19 22:24:37 +0530

    Update node.js

--- node.js
+++ node.js
@@ -4,19 +4,12 @@ const MY_PHONE_NUMBER = '94742381405';
 
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: "shana-bot" }),
-   puppeteer: {
-    headless: true,
-    args: [
-        '--no-sandbox', 
-        '--disable-setuid-sandbox', 
-        '--disable-dev-shm-usage', 
-        '--disable-accelerated-2d-canvas', 
-        '--no-first-run', 
-        '--no-zygote', 
-        '--single-process', 
-        '--disable-gpu'
-    ]
-}
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
+    }
+});
+
 client.on('ready', () => {
     console.log('✅ බොට් සාර්ථකව සම්බන්ධ විය!');
 });
