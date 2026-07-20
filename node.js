b4542ddff8beafdafd89ccfb6b69b0d060a0c4e4
kaviyabanuka28-source <kaviyabanuka28@gmail.com>
2026-07-20 07:54:16 +0530

    Update node.js

--- node.js
+++ node.js
@@ -23,19 +23,8 @@ function canReply(userId) {
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        headless: true,
-        args: [
-            '--no-sandbox', 
-            '--disable-setuid-sandbox', 
-            '--disable-dev-shm-usage', 
-            '--disable-accelerated-2d-canvas', 
-            '--no-first-run', 
-            '--no-zygote', 
-            '--single-process', 
-            '--disable-gpu',
-            '--disable-extensions'
-        ],
-        executablePath: '/usr/bin/chromium'
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+        executablePath: '/usr/bin/google-chrome-stable'
     },
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 });
