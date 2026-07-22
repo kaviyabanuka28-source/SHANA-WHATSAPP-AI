92692255d157fd9287ef28c4b35e15450122c048
kaviyabanuka28-source <kaviyabanuka28@gmail.com>
2026-07-22 16:08:42 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    botName: "SHANA",
+    version: "8.0.0",
+    cooldownTime: 20 * 60 * 1000,
+    sessionDir: "session",
+    port: process.env.PORT || 8080
+};
+
+module.exports = config;
